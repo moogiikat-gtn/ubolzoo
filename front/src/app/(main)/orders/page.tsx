d4ed39501704f739521d0f128a37a77933c54db4
moogiikat-gtn <100661374+moogiikat-gtn@users.noreply.github.com>
2024-06-08 18:03:00 +0900

    done

--- front/src/app/(main)/orders/page.tsx
+++ front/src/app/(main)/orders/page.tsx
@@ -3,7 +3,7 @@ import { Invitation } from "./invitation";
 
 const OrdersPage: React.FC = () => {
   return (
-    <main className="p-12">
+    <main className="p-12 flex flex-col gap-y-4">
       <div className="card lg:card-side bg-base-100 shadow-xl">
         <figure className="relative h-[250px] w-1/3">
           <Image
@@ -21,6 +21,27 @@ const OrdersPage: React.FC = () => {
           </p>
           <div className="card-actions justify-end">
             <Invitation />
+            <button className="btn btn-primary text-white">Purchase</button>
+          </div>
+        </div>
+      </div>
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        <figure className="relative h-[250px] w-1/3">
+          <Image
+            fill
+            src="https://www.toursmongolia.com/uploads/Choijin%20Lama%20Temple%20Ulaanbaatar%20Mongolia.jpg"
+            alt="Album"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Order #12345679</h2>
+          <h3 className="text-xl font-semibold">Bogd Khan</h3>
+          <p>
+            A historic site featuring the winter palace of the last Mongolian
+            emperor. Offers cultural insights and a beautiful setting.
+          </p>
+          <div className="card-actions justify-end">
+            <button  className="btn bg-gray-500 text-white">Accepted</button>
             <button className="btn btn-primary text-white">Purchase</button>
           </div>
         </div>
