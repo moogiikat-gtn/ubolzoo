141ac7aeb4aa5c68835e7a22cc65f112c936ba3b
buyna-gtn <b.bekhbayar@gtn.co.jp>
2024-06-08 15:36:56 +0800

    remove menu bar

--- front/src/app/components/header.tsx
+++ front/src/app/components/header.tsx
@@ -26,40 +26,6 @@ export const Header: React.FC = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h7"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a href="/news">News</a>
-            </li>
-            <li>
-              <a href="/plans">Plans</a>
-            </li>
-            <li>
-              <a href="/dates">Dates</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className="navbar-center">
         <Link href="/" className="btn btn-ghost text-xl">
           <Logo />
         </Link>
