0b20799385f85dae5d7a7f5bc7ae8a0243f3964c
buyna-gtn <b.bekhbayar@gtn.co.jp>
2024-06-08 10:33:59 +0800

    add hero section

---
+++ front/src/app/components/hero.tsx
@@ -0,0 +1,12 @@
+export const Hero: React.FC = () => {
+  return (
+    <div className="h-[calc(100vh-128px)] bg-red-500 bg-center bg-cover bg-[linear-gradient(to_right,rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(/background.jpeg)] rounded-2xl mx-6 my-8 relative flex flex-col gap-y-8 justify-center px-16 items-start">
+      <h1 className="text-white text-6xl tracking-wider font-semibold w-1/2 leading-tight">
+        Discover the Heart of Ulaanbaatar.
+      </h1>
+      <button className="btn btn-primary font-bold text-slate-50">
+        See plans for your next date
+      </button>
+    </div>
+  );
+};
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { getNews } from "@/services/redis/getNews";
 import { use } from "react";
 import { Header } from "./components/header";
+import { Hero } from "./components/hero";
 
 export default function Home() {
   const response = use(getNews());
@@ -10,14 +11,7 @@ export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Header />
-      <div className="h-[calc(100vh-128px)] bg-red-500 bg-center bg-cover bg-[linear-gradient(to_right,rgba(0,0,0,0.6),rgba(0,0,0,0.6)),url(/background.jpeg)] rounded-2xl mx-6 my-8 relative flex flex-col gap-y-8 justify-center px-16 items-start">
-        <h1 className="text-white text-6xl tracking-wider font-semibold w-1/2 leading-tight">
-          Discover the Heart of Ulaanbaatar.
-        </h1>
-        <button className="btn btn-primary font-bold text-slate-50">
-          See plans for your next date
-        </button>
-      </div>
+      <Hero />
     </main>
   );
 }
