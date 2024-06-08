6093b485709c839252c5956ffa8b9c3f229f8dde
moogiikat-gtn <100661374+moogiikat-gtn@users.noreply.github.com>
2024-06-08 17:59:05 +0900

    update

--- front/src/app/(main)/cart/page.tsx
+++ front/src/app/(main)/cart/page.tsx
@@ -11,7 +11,7 @@ export default function Orders() {
     <div className="px-8 flex flex-col sm:gap-y-8 gap-y-6 text-slate-700 py-6">
       <div>
         <p className="text-[#15aabf]">Need a date idea?</p>
-        <h2 className="text-3xl sm:text-5xl font-bold">Your order</h2>
+        <h2 className="text-3xl sm:text-5xl font-bold">Your cart</h2>
       </div>
       <Locations locations={data.filter((item) => item.category === "order")} />
       <div className="flex justify-end gap-x-2">
