19d0655014b1c11906cb4bfe200384cc2fb49906
buyna-gtn <b.bekhbayar@gtn.co.jp>
2024-06-08 11:23:04 +0800

    responsive

--- front/src/app/components/news/news.tsx
+++ front/src/app/components/news/news.tsx
@@ -1,13 +1,13 @@
 import { NewsCarousel } from "./news_carousel";
-import type { NewsSchema } from "../../schemas/news";
+import type { NewsSchema } from "@/app/schemas/news";
 
 type Props = {
   news: NewsSchema[];
 };
 export const News: React.FC<Props> = ({ news }) => {
   return (
-    <div className="px-8 flex flex-col gap-y-8 bg-primary/90 py-12 text-slate-100">
-      <h2 className="text-2xl sm:text-5xl font-bold">News</h2>
+    <div className="px-8 flex flex-col sm:gap-y-8 gap-y-6 bg-primary/90 py-10 sm:py-12 text-slate-100">
+      <h2 className="text-3xl sm:text-5xl font-bold">News</h2>
       <NewsCarousel news={news} />
     </div>
   );
